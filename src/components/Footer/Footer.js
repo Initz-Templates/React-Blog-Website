d5ffc96fd92267113ac9b72dff9bcb885174a0d8
Utkarsh Katiyar <utkarshkatiyar688@gmail.com>
2024-05-22 16:30:32 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, {memo} from 'react'
+
+function Footer() {
+  return (
+    <div className="card-footer text-muted text-center font-weight-bolder p-5 m-2">
+      © 2024, Made with
+❤️
+by
+initializ inc.
+    </div>
+  )
+}
+export default memo(Footer)
